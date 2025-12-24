22315f8dd079548c4bece1be53775fe497749e3c
felipeolv11 <felipeeh1524@gmail.com>
2025-12-24 19:16:53 -0300

    configurar ef core e mysql ao projeto

---
+++ Catalogo API/Context/CatalogoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Catalogo_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo_API.Context;
+
+public class CatalogoDbContext : DbContext
+{
+    public CatalogoDbContext(DbContextOptions<CatalogoDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+}
--- Catalogo API/Program.cs
+++ Catalogo API/Program.cs
@@ -1,3 +1,6 @@
+using Catalogo_API.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+string? mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<CatalogoDbContext>(options =>
+                        options.UseMySql(mySqlConnection, 
+                        ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build();
 
